3744a07149fecc39c786158ab9e96b05eae974ea
Petr <pmarfutenko@yandex.ru>
2021-09-10 16:44:23 +0300

    add functions

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+"use strict";
 
-const numberOfFilms = prompt('Сколько фиьмов вы посмотрели', '');
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = prompt('Сколько фиьмов вы посмотрели', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('Сколько фиьмов вы посмотрели', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,13 +20,44 @@ const personalMovieDB = {
     private: false
 };
 
-const filmName = prompt('один изпросмотренных фильмов',''),
-a = prompt('На сколько его оцените',''),
-filmName1 = prompt('один изпросмотренных фильмов',''),
-b = prompt('На сколько его оцените','');
+function rememberMyFilms(){
+    for(let i = 0; i < 3 ; i++){
+        const filmName  = prompt('один изпросмотренных фильмов',''),
+        a = prompt('На сколько его оцените','');
 
+        if (filmName != null && filmName.length < 50 && filmName != '' && a != null && a != ''){
+            personalMovieDB.movies[filmName] = a;
+            console.log('Done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}  
 
-personalMovieDB.movies[filmName] = a;
-personalMovieDB.movies[filmName1] = b;
+rememberMyFilms();
 
-console.log(personalMovieDB);
+function detectPersonaLevel(){
+    if(personalMovieDB.count < 10){
+        console.log('Мало фильмов');
+    } else if(personalMovieDB.count > 10 && personalMovieDB.movies.count < 30){
+        console.log('Вы классический зритель');
+    } else if(personalMovieDB.count > 30){
+        console.log('Вы киноман');
+    } else {
+        console.log('Ошибка');
+    }
+}
+
+detectPersonaLevel();
+
+function showMyDB(){
+    if(!personalMovieDB.private){
+        console.log(personalMovieDB);
+    }
+    else{
+        console.log('Error');
+    }
+}
+
+showMyDB();
---
+++ tryScript.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const str = "teSt";
+
+
+
+// console.log(str[2] = 'd');
+
+
+console.log(str.toUpperCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello, World";
+
+console.log(logg.substr(7, 5));
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+
+const test = "12.2px";
+
+console.log(parseFloat(test));
